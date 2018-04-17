50a046224cd44fd7d256f269aeca9fd89377d8f2
Nicolas Marini <nmarini44@gmail.com>
2018-04-17 23:59:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 3 && % 5 == 0
     "FizzBuzz"
-  elsif int % 5 == 0
-    "Buzz"
-elsif int % 3 == 0
-"Fizz"
+  elsif int % 3 == 0
+    "Fizz"
+elsif int % 5 == 0
+"Buzz"
 
 end
 end
